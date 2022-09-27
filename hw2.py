55043e16a2b4f692af1032ee14f8b9cd8599f560
Tamyrlan <tshurenov@gmail.com>
2022-09-27 16:31:04 +0600

    delete2

--- hw2.py
+++
@@ -1,6 +0,0 @@
-print("x y z")
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            if (not(x or y or z)) == (not x and not y and not z):
-                print (f"¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - True, when х={x},y= {y}, z= {z}")
