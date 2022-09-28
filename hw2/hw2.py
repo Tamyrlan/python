25818427eef57e6322e02d852c146a84aa39a469
Tamyrlan <tshurenov@gmail.com>
2022-09-28 18:43:19 +0600

    homework 2

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,5 +1,12 @@
-numbers=[]
-n=int(input('Enter n:'))
-for i in range(0,n+1):
-    numbers.append(i*(i+1))
+# 2. Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+# 1 - 1 * 1, 2 - 1 * 2, 3 - 1 * 2 * 3, 4 - 1 * 2 * 3 * 4 и т.д.
+# - 4 -> [1, 2, 6, 24]
+# - 6 -> [1, 2, 6, 24, 120, 720]
+numbers = []
+n = int(input('Enter n:'))
+factorial = 1
+for i in range(1, n+1):
+    factorial = factorial * i
+    numbers.append(factorial)
 print(numbers)
