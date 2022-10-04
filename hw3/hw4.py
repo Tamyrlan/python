7652c639672c38082e9ee9d74068d126702dca84
Tamyrlan <tshurenov@gmail.com>
2022-10-04 15:53:05 +0600

    task4

--- hw3/hw4.py
+++ hw3/hw4.py
@@ -13,17 +13,29 @@
 # out
 # [9.26, 8.5, 1.14]
 # Min: 0.14, Max: 0.5. Difference: 0.36
-from random import randrange, uniform
-num = int(input(':'))
-n=[]
-for i in range(1,num):
-    n.append(round(uniform(1.0,99.9),2))
-print(n)
-for i in n:
-    max=0
-    min=0
-    if max<n[i]:
-        max=n[i]
-    elif max>n[i]:
-        min=n[i]
-print(min,max)
+from random import uniform
+def random_list(n):
+    a_list=[]
+    if n<=0:
+        print('error,wrong number')
+        return 0
+    
+    for i in range(n):
+        a_list.append(round(uniform(1,n),2))
+    return a_list
+
+def max_min_diff(a_list):
+    max_num = a_list[0]%1
+    min_num = a_list[0]%1
+    for j in range(1,len(a_list)):
+        num = round(a_list[j]%1,2)
+        if num>max_num:
+            max_num=num
+        elif num<min_num:
+            min_num=num
+    result = round(max_num-min_num,2)
+    return (f'min_elem={min_num} max_elem = {max_num} The difference between them = {result}')
+
+result = random_list(int(input('x:')))
+print(result)
+print(max_min_diff(result))
